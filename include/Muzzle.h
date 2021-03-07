931e2aa8756c638c5db0b8ea9020b6eaac2c5e59
OkiStuff <probro1802@gmail.com>
2021-03-07 06:49:51 -0500

    Added raylibExtras library

--- include/Muzzle.h
+++ include/Muzzle.h
@@ -2,7 +2,16 @@
 #include "core/Applet.h"
 #include "textures/Sprite2D.h"
 #include "textures/Sprite3D.h"
-#include "core/Scene.h"
 #include "raylib.h"
 
 void QuitMuzzle();
+
+/*
+    Muzzle Alpha 0.1
+        PATCH NOTES
+            Added and Removed "Scenes"
+            Added Sprite2D
+            Added Sprite3D
+            Added Applet
+            Added and Update Loop
+*/
