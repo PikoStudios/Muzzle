8f8baf7228da8dd5ea6f82a19c3f50831cd0a277
OkiStuff <probro1802@gmail.com>
2021-06-04 09:41:04 -0400

    New Example

--- examples/circle/main.c
+++ examples/circle/main.c
@@ -9,12 +9,16 @@ Applet blank_window;
 void OnAppletUpdate()
 {
     tint my_color = {84, 84, 84, 255};
+    float rad = 5.0f;
 
     while (keep_applet(blank_window.window_handle))
     {
         begin_drawing();
             clear_screen(my_color);
             draw_rectangle(50,50,120,120,WHITE);
+
+            rad += 0.5f;
+            draw_circle(500,500, rad, USAF_BLUE);
         end_drawing(&blank_window);
     }
     
