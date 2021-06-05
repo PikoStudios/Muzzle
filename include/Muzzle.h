43946046e59d9cfe7a431d962808de71b6d65e74
OkiStuff <probro1802@gmail.com>
2021-06-05 16:16:10 -0400

    Updated CMakeLists.txt and added sprite rendering

--- include/Muzzle.h
+++ include/Muzzle.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "core/Applet.h"
-//#include "textures/Sprite2D.h"
-//#include "textures/Sprite3D.h"
 #include "core/vector.h"
 #include "core/Drawing.h"
 #include "core/tint.h"
+#include "core/Sprite.h"
 #include "shapes/Rectangle.h"
 #include "shapes/Circle.h"
 #include "core/Input.h"
---
+++ include/core/Sprite.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../backend.h"
+#include "../../deps/stb/stb_image.h"
+#include "Error.h"
+#include "tint.h"
+#include "vector.h"
+
+struct _mz_sprite
+{
+    unsigned int sprite_id;
+    int width,height;
+};
+
+/*
+Muzzle Sprite
+=============
+unsigned int sprite_id
+int width, height
+*/
+typedef struct _mz_sprite sprite;
+
+sprite load_sprite(const char *filepath);
+void unload_sprite(sprite *data);
+void draw_sprite(sprite *data, int x, int y, float scale, float rotation, tint color_drawn);
+void draw_sprite_vec2(sprite *data, vec2 pos, float scale, float rotation, tint color_drawn);
