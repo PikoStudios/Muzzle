5aa632f8d1bf69ea756baf910063fc2ac3477279
OkiStuff <probro1802@gmail.com>
2021-08-01 19:44:39 -0400

    Added New Colors
    Colors:\nWHITE GRAY RED ORANGE GREEEN BLUE PURPLE PINK MAGENTA YELLOW USAF_BLUE BLACK

--- include/core/tint.h
+++ include/core/tint.h
@@ -19,6 +19,13 @@ tint rgb(unsigned int red,unsigned int green,unsigned int blue,unsigned int alph
 
 #define WHITE       (tint){255,255,255,255}
 #define GRAY        (tint){84, 84, 84, 255}
+#define RED         (tint){255,50,100,255}
+#define ORANGE      (tint){255,153,51,255}
+#define GREEN       (tint){102,255,102,255}
+#define BLUE        (tint){0,128,255,255}
+#define PURPLE      (tint){178,102,255,255}
+#define PINK        (tint){255,204,255,255}
+#define MAGENTA     (tint){255,102,255,255}
 #define YELLOW      (tint){255,255,51,255}
 #define USAF_BLUE   (tint){0,48,143,255}
 #define BLACK       (tint){0,0,0,255}
