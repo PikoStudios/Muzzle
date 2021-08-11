4cce6055ae340a0ccff2fa167be78625aaa9c075
OkiStuff <probro1802@gmail.com>
2021-08-11 19:07:14 -0400

    added vec2_d and vec3_d (Vectors with doubles)

--- include/core/vector.h
+++ include/core/vector.h
@@ -12,3 +12,18 @@ typedef struct vec3
     float y;
     float z;
 } vec3;
+
+// Vector2 with doubles
+typedef struct vec2_d
+{
+    double x;
+    double y;
+};
+
+// Vector3 with doubles
+typedef struct vec3_d
+{
+    double x;
+    double y;
+    double z;
+} vec3;
