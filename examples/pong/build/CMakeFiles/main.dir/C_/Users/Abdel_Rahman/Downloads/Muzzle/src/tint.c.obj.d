db927a0ae85314d0651ff937f49054725f02a1bd
unknown <abdonaggar77@gmail.com>
2021-08-15 19:09:42 +0200

    Changed name of folder and title. Removed pong/build. Improved formatting.

--- examples/pong/build/CMakeFiles/main.dir/C_/Users/Abdel_Rahman/Downloads/Muzzle/src/tint.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/C_/Users/Abdel_Rahman/Downloads/Muzzle/src/tint.c.obj: \
- C:\Users\Abdel\ Rahman\Downloads\Muzzle\src\tint.c \
- C:/Users/ABDELR~1/DOWNLO~1/Muzzle/include/core/tint.h
