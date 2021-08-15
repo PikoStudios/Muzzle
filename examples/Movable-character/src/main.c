2426351b580105b2b88745c45396e49eb6e75c30
unknown <abdonaggar77@gmail.com>
2021-08-15 04:20:44 +0200

    Added example where users can move around a square with WASD

---
+++ examples/Movable-character/src/main.c
@@ -0,0 +1,44 @@
+#define MUZZLE_DEPS 
+#include <Muzzle.h>
+#include <stdio.h>
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
+
+Applet applet;
+
+void OnAppletUpdate()
+{
+   rectangle player = {
+        .height = 50,
+        .width = 50,
+        .x = 225,
+        .y = 225
+    };
+    tint my_color = {84, 84, 84, 255};
+
+    while (keep_applet(applet.window_handle))
+    {
+        begin_drawing();
+            clear_screen(my_color);
+                        
+                        if(key_down(&applet, KEY_W)&&player.y!=5) player.y -=5;
+                        else if(key_down(&applet, KEY_S)&&player.y!=495&&player.y!=490) player.y +=5;
+  
+                         else if(key_down(&applet, KEY_A)&&player.x!=5) player.x -=5;
+                         else if(key_down(&applet, KEY_D)&&player.x!=495&&player.x!=490) player.x +=5;
+                        
+
+                        draw_rectangle_rec(player,WHITE);
+        end_drawing(&applet);
+    }
+    
+}
+
+int main(void)
+{
+    applet = InitializeApplet(SCREEN_WIDTH, SCREEN_HEIGHT, "Muzzle [CORE] - Blank Window", MUZZLE_FALSE, MUZZLE_TRUE);
+    StartApplet(&applet);
+
+    QuitMuzzle(applet);
+    return 0;
+}
---
+++ examples/pong/build/CMakeFiles/main.dir/C_/Users/Abdel_Rahman/Downloads/Muzzle/src/callback.c.obj.d
@@ -0,0 +1,17 @@
+CMakeFiles/main.dir/C_/Users/Abdel_Rahman/Downloads/Muzzle/src/callback.c.obj: \
+ C:\Users\Abdel\ Rahman\Downloads\Muzzle\src\callback.c \
+ C:/Users/ABDELR~1/DOWNLO~1/Muzzle/include/core/callback.h \
+ C:/Users/ABDELR~1/DOWNLO~1/Muzzle/include/backend.h \
+ C:/Users/ABDELR~1/DOWNLO~1/Muzzle/deps/glfw/include/GLFW/glfw3.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/stddef.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/stddef.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/crtdefs.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/_mingw.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/vadefs.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/stdint.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/stdint.h \
+ C:/Users/ABDELR~1/DOWNLO~1/MINGW-~1/I686-8~1.0-P/mingw32/i686-w64-mingw32/include/GL/gl.h
