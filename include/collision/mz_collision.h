6f3dcd4775c29f11f911b9e595adae7b553cfcf9
OkiStuff <probro1802@gmail.com>
2021-09-30 10:52:16 -0400

    update submodules

---
+++ include/collision/mz_collision.h
@@ -0,0 +1,13 @@
+#ifndef MZ_COLLISION_H
+#define MZ_COLLISION_H
+
+#ifdef MZ_DEPS_COLLISION
+    #define MZ_SR_RESOLVE_IMPLEMENTION
+#endif
+
+
+#include "../../deps/sr_resolve/sr_resolve.h"
+
+
+
+#endif // MZ_COLLISION_H
