8a674b2deaeb2176d724fe65ea207afd2a2de7c0
OkiStuff <probro1802@gmail.com>
2021-10-05 19:09:14 -0400

    add new cmakelists to examples, and added update_all_cmakelists.ps1

---
+++ modules/update_all_cmakelists.ps1
@@ -0,0 +1,10 @@
+$base = "..\CMakeLists.txt"
+$example_dir = "..\examples"
+
+$examples = "..\examples\blank_window\", "..\examples\circle\", "..\examples\collision\", "..\examples\input\", "..\examples\moveable_square\", "..\examples\pong\", "..\examples\rectangle\", "..\examples\resizing\", "..\examples\sprites\", "..\examples\text\"
+
+Remove-Item -Path $example_dir\*\CMakeLists.txt -Verbose
+
+for ($i = 0; $i -lt $examples.Count; $i++) {
+    Copy-Item $base $examples[$i] -Verbose
+}
