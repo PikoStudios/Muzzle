a9391e1fa2b99307981cf64b33d6061947174c7f
OkiStuff <probro1802@gmail.com>
2021-10-17 15:45:38 -0400

    Added hex_to_rgb();

---
+++ examples/hex_to_rgb/main.c
@@ -0,0 +1,29 @@
+#define MUZZLE_DEPS
+#include <Muzzle.h>
+#include <stdio.h>
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+Applet applet;
+
+void OnAppletUpdate()
+{
+    unsigned int hex[6] = {0xF, 0xF, 0x0, 0x0, 0x0, 0x0};
+    tint color = hex_to_rgb(hex, 255);
+    while (keep_applet(applet.window_handle))
+    {
+        begin_drawing();
+            clear_screen(color);
+        end_drawing(&applet);
+    }
+    
+}
+
+int main(void)
+{
+    applet = InitializeApplet(SCREEN_WIDTH, SCREEN_HEIGHT, "Muzzle [CORE] - Blank Window", MUZZLE_FALSE, MUZZLE_TRUE);
+    StartApplet(&applet);
+
+    QuitMuzzle(applet);
+    return 0;
+}
--- src/tint.c
+++ src/tint.c
@@ -3,9 +3,9 @@
 
 tint hex_to_rgb(unsigned int hex[6], unsigned int alpha)
 {
-    unsigned int r = hex[0] + hex[1];
-    unsigned int b = hex[2] + hex[3];
-    unsigned int g = hex[4] + hex[5];
+    unsigned int r = hex[0] * hex[1];
+    unsigned int b = hex[2] * hex[3];
+    unsigned int g = hex[4] * hex[5];
 
     return (tint)
     {
