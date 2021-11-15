8f251c45234822299f95a8b3f0d240a8309160e7
OkiStuff <probro1802@gmail.com>
2021-11-15 17:35:36 -0500

    Added Shared Variables

--- include/Muzzle.h
+++ include/Muzzle.h
@@ -14,6 +14,12 @@
     #include "core/Audio.h"
 #endif
 
+#ifndef MUZZLE_SHARED_VARIABLE_ARRAY_SIZE
+    #define MUZZLE_SHARED_VARIABLE_ARRAY_SIZE 5
+#endif
+
+static void* __muzzle_shared_variable_array[MUZZLE_SHARED_VARIABLE_ARRAY_SIZE];
+
 #include "core/vector.h"
 #include "core/Drawing.h"
 #include "core/tint.h"
@@ -26,3 +32,7 @@
 #include <stdlib.h>
 
 void QuitMuzzle(Applet applet);
+
+void create_shared_variable(void* variable, int index);
+void remove_shared_variable(int index);
+void* get_shared_variable(int index);
--- src/Muzzle.c
+++ src/Muzzle.c
@@ -8,3 +8,18 @@ void QuitMuzzle(Applet applet)
     log_status(STATUS_INFO, "Terminating OpenGL Context");
     glfwTerminate();
 }
+
+void create_shared_variable(void* variable, int index)
+{
+    __muzzle_shared_variable_array[index] = variable;
+}
+
+void remove_shared_variable(int index)
+{
+    __muzzle_shared_variable_array[index] = NULL;
+}
+
+void* get_shared_variable(int index)
+{
+    return __muzzle_shared_variable_array[index];
+}
