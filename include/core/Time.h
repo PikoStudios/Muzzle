6fc4fe1dec7a95fa73b6ed19ac6454ed6d19f2af
OkiStuff <probro1802@gmail.com>
2022-02-19 14:09:40 -0500

    remove time spec

--- include/core/Time.h
+++ include/core/Time.h
@@ -11,12 +11,6 @@ void update_delta_time();
 double get_delta_time();
 float get_delta_time_float();
 
-struct __time_spec
-{ 
-    __int64     tv_sec;
-    __int32     tv_nsec;
-};
-
 #ifdef __cplusplus
 }
 #endif
