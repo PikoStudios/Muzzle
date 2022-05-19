09e97f6a0b404fc0716e20ccebf52b9839a24b39
OkiStuff <probro1802@gmail.com>
2022-05-19 17:16:35 -0400

    MuzzleMath v2.3
    Added MZ_MATH_STANDALONE macro, Added quadatic fomula functions, Added true sqroot functions

--- include/MuzzleMath.h
+++ include/MuzzleMath.h
@@ -1,8 +1,37 @@
 #pragma once
 
+// MuzzleMath v2.3
+
+/*
+*   CHANGE LOG (2.3)
+*   - Added MZ_MATH_STANDALONE macro
+*   - Added quadratic formula functions
+*   - Added true sqroot functions
+*/
+
 #include <math.h>
 
-// MuzzleMath v2
+#ifndef MZ_MATH_STANDALONE
+    #include "core/vector.h"
+
+    #define MZ_MATH_VECTOR2 vec2
+    #define MZ_MATH_VECTOR2_DOUBLE vec2_d
+#else
+    typedef struct mz_math_vec2
+    {
+        float x;
+        float y;
+    } mz_math_vec2; // not named vec2 to avoid name conflicts
+
+    typedef struct mz_math_vec2_d
+    {
+        double x;
+        double y;
+    } mz_math_vec2_d; // not named vec2_d to avoid name conflicts
+
+    #define MZ_MATH_VECTOR2 mz_math_vec2
+    #define MZ_MATH_VECTOR2_DOUBLE mz_math_vec2_d
+#endif // MZ_MATH_STANDALONE
 
 #define PI  3.14159
 #define DEG2RAD     (PI / 180)
@@ -14,6 +43,20 @@ float mz_math_lerp(float start, float end, float amount);
 
 float mz_math_normalize(float val, float start, float end);
 float mz_math_remap(float val, float input_start, float input_end, float output_start, float output_end);
+
+// Not allocated. No need to free()
+float** mz_math_quadratic_formula_array(float a, float b, float c);
+MZ_MATH_VECTOR2 mz_math_quadratic_formula_vec2(float a, float b, float c); // MZ_MATH_VECTOR2 is either vec2 or mz_math_vec2. both are the same.
+
+// Not allocated. No need to free()
+double** mz_math_quadratic_formula_array_double(double a, double b, double c);
+MZ_MATH_VECTOR2_DOUBLE mz_math_quadratic_formula_vec2_d(double a, double b, double c);
+
+double** mz_math_true_sqroot(double val);
+MZ_MATH_VECTOR2_DOUBLE mz_math_true_sqroot_vec2_d(double val);
+
+float** mz_math_true_sqroot_float(float val);
+MZ_MATH_VECTOR2 mz_math_true_sqroot_vec2(float val);
 
 #ifdef MZ_MATH_IMPLEMENTION
 
@@ -43,6 +86,72 @@ float mz_math_normalize(float val, float start, float end)
 float mz_math_remap(float val, float input_start, float input_end, float output_start, float output_end)
 {
     return (val - input_start) / (input_end - input_start) * (output_end - output_start) + output_start;
+}
+
+float** mz_math_quadratic_formula_array(float a, float b, float c)
+{
+    float[2] res;
+    float sqroot_val = sqrt((b*b) - (4 * a * c));
+
+    res[0] = (-b + sqroot_val) / (2 * a);
+    res[1] = (-b - sqroot_val) / (2 * a);
+
+    return res;
+}
+
+MZ_MATH_VECTOR2 mz_math_quadratic_formula_vec2(float a, float b, float c)
+{
+    float[2] res = mz_math_quadratic_formula_array(a,b,c);
+
+    return (MZ_MATH_VECTOR2){.x = res[0], .y = res[1]};
+}
+
+double** mz_math_quadratic_formula_array_double(double a, double b, double c)
+{
+    double[2] res;
+    double sqroot_val = sqrt((b*b) - (4 * a * c));
+
+    res[0] = (-b + sqroot_val) / (2 * a);
+    res[1] = (-b - sqroot_val) / (2 * a);
+
+    return res;   
+}
+
+MZ_MATH_VECTOR2_DOUBLE mz_math_quadratic_formula_vec2_d(double a, double b, double c)
+{
+    double[2] res = mz_math_quadratic_formula_array_double(a,b,c);
+
+    return (MZ_MATH_VECTOR2_D){.x = res[0], .y = res[1]};
+}
+
+double** mz_math_true_sqroot(double val)
+{
+    double sq = sqrt(val);
+    double[] res = {sq, -sq};
+
+    return res;
+}
+
+MZ_MATH_VECTOR2_D mz_math_true_sqroot_vec2_d(double val)
+{
+    double[2] res = mz_math_true_sqroot(val);
+
+    return (MZ_MATH_VECTOR2_D){.x = res[0], .y = res[1]};
+}
+
+float** mz_math_true_sqroot_float(float val)
+{
+    float sq = sqrtf(val);
+    float[] res = {sq, -sq};
+
+    return res;
+}
+
+MZ_MATH_VECTOR2 mz_math_true_sqroot_vec2(float val)
+{
+    float[2] = mz_math_true_sqroot_float(val);
+
+    return (MZ_MATH_VECTOR2){.x = res[0], .y = res[1]};
 }
 
 #endif // MZ_MATH_IMPLEMENTION
