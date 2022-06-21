1f692120af36bd2718e49dd056b7da952433ad9e
OkiStuff <probro1802@gmail.com>
2022-06-21 16:09:33 -0400

    added delta functionality to particles system

--- include/core/Particles.h
+++ include/core/Particles.h
@@ -11,12 +11,15 @@ struct _mz_particle
     vec2 velocity;
     vec2 acceleration;
 
-    float lifespan;
-    int dead;
+    float lifespan; // recommended default is 255
+    int dead; // set to 0 or MUZZLE_FALSE
 
+    float timer; // set to 0.0f, If not using `update_particle_delta`, ignore this.
     float slasher_amount; // How much to slash off the lifespan every update
+    float slasher_time; // If not using `update_particle_delta`, ignore this.
 };
 
 typedef struct _mz_particle particle;
 
 void update_particle(particle* obj);
+void update_particle_delta(particle* obj, float delta); // Will decrease lifespan every second
