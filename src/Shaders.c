107ca1d357c082ef853986f46b20474d84df3d76
OkiStuff <probro1802@gmail.com>
2022-08-23 08:37:24 -0400

    modern renderer rehaul

--- src/Shaders.c
+++ src/Shaders.c
@@ -61,42 +61,37 @@ void unload_shader_definition(shader_def* definition)
 
 shader create_shader(shader_def* definition)
 {
-    #ifdef MUZZLE_USE_MODERN_RENDERER
-        unsigned int id = rlLoadShaderCode(definition->vertex, definition->fragment);
-        int *locations = MZ_CALLOC(RL_MAX_SHADER_LOCATIONS, sizeof(int));
+    unsigned int id = rlLoadShaderCode(definition->vertex, definition->fragment);
+    int *locations = MZ_CALLOC(RL_MAX_SHADER_LOCATIONS, sizeof(int));
 
-        if (locations == NULL)
-        {
-            log_status(STATUS_FATAL_ERROR, "Failed to allocate memory for shader");
-        }
+    if (locations == NULL)
+    {
+        log_status(STATUS_FATAL_ERROR, "Failed to allocate memory for shader");
+    }
 
-        for (int i = 0; i < RL_MAX_SHADER_LOCATIONS; i++) locations[i] = -1;
-        if (id > 0)
-        {
-            locations[RL_SHADER_LOC_VERTEX_COLOR] =         rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_POSITION);
-            locations[RL_SHADER_LOC_VERTEX_TEXCOORD01] =    rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_TEXCOORD);
-            locations[RL_SHADER_LOC_VERTEX_TEXCOORD02] =    rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_TEXCOORD2);
-            locations[RL_SHADER_LOC_VERTEX_NORMAL] =        rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_NORMAL);
-            locations[RL_SHADER_LOC_VERTEX_TANGENT] =       rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_TANGENT);
-            locations[RL_SHADER_LOC_VERTEX_COLOR] =         rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_COLOR);
-    
-            locations[RL_SHADER_LOC_MATRIX_MVP] =           rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_MVP);
-            locations[RL_SHADER_LOC_MATRIX_VIEW] =          rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_VIEW);
-            locations[RL_SHADER_LOC_MATRIX_PROJECTION] =    rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_PROJECTION);
-            locations[RL_SHADER_LOC_MATRIX_MODEL] =         rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_MODEL);
-            locations[RL_SHADER_LOC_MATRIX_NORMAL] =        rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_NORMAL);
-    
-            locations[RL_SHADER_LOC_COLOR_DIFFUSE] =        rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_COLOR);
-            locations[RL_SHADER_LOC_MAP_DIFFUSE] =          rlGetLocationUniform(id, MZ_SHADER_SAMPLER2D_NAME_TEXTURE0);  // SHADER_LOC_MAP_ALBEDO
-            locations[RL_SHADER_LOC_MAP_SPECULAR] =         rlGetLocationUniform(id, MZ_SHADER_SAMPLER2D_NAME_TEXTURE1); // SHADER_LOC_MAP_METALNESS
-            locations[RL_SHADER_LOC_MAP_NORMAL] =           rlGetLocationUniform(id, MZ_SHADER_SAMPLER2D_NAME_TEXTURE2);
-        }
+    for (int i = 0; i < RL_MAX_SHADER_LOCATIONS; i++) locations[i] = -1;
+    if (id > 0)
+    {
+        locations[RL_SHADER_LOC_VERTEX_COLOR] =         rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_POSITION);
+        locations[RL_SHADER_LOC_VERTEX_TEXCOORD01] =    rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_TEXCOORD);
+        locations[RL_SHADER_LOC_VERTEX_TEXCOORD02] =    rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_TEXCOORD2);
+        locations[RL_SHADER_LOC_VERTEX_NORMAL] =        rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_NORMAL);
+        locations[RL_SHADER_LOC_VERTEX_TANGENT] =       rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_TANGENT);
+        locations[RL_SHADER_LOC_VERTEX_COLOR] =         rlGetLocationAttrib(id, MZ_SHADER_ATTRIB_NAME_COLOR);
 
-        return (shader) { locations, id };
-    #else
-        log_status(STATUS_ERROR, "Shaders are only supported on the modern renderer");
-        return (shader){0};
-    #endif
+        locations[RL_SHADER_LOC_MATRIX_MVP] =           rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_MVP);
+        locations[RL_SHADER_LOC_MATRIX_VIEW] =          rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_VIEW);
+        locations[RL_SHADER_LOC_MATRIX_PROJECTION] =    rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_PROJECTION);
+        locations[RL_SHADER_LOC_MATRIX_MODEL] =         rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_MODEL);
+        locations[RL_SHADER_LOC_MATRIX_NORMAL] =        rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_NORMAL);
+
+        locations[RL_SHADER_LOC_COLOR_DIFFUSE] =        rlGetLocationUniform(id, MZ_SHADER_UNIFORM_NAME_COLOR);
+        locations[RL_SHADER_LOC_MAP_DIFFUSE] =          rlGetLocationUniform(id, MZ_SHADER_SAMPLER2D_NAME_TEXTURE0);  // SHADER_LOC_MAP_ALBEDO
+        locations[RL_SHADER_LOC_MAP_SPECULAR] =         rlGetLocationUniform(id, MZ_SHADER_SAMPLER2D_NAME_TEXTURE1); // SHADER_LOC_MAP_METALNESS
+        locations[RL_SHADER_LOC_MAP_NORMAL] =           rlGetLocationUniform(id, MZ_SHADER_SAMPLER2D_NAME_TEXTURE2);
+    }
+
+    return (shader) { locations, id };
 }
 void unload_shader(shader* shader)
 {
