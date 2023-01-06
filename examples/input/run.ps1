bd7e88971aacf97fdb5bfb2020322c211cdcdabd
OkiStuff <probro1802@gmail.com>
2023-01-05 20:58:35 -0500

    remove ignored compilation scripts

--- examples/input/run.ps1
+++
@@ -1,7 +0,0 @@
-mkdir build
-cd build
-cmake .. -G "MinGW Makefiles" -DCMAKE_C_COMPILER=C:/raylib/mingw/bin/gcc.exe -DCMAKE_MAKE_PROGRAM=C:\raylib\mingw\bin\mingw32-make.exe
-C:\raylib\mingw\bin\mingw32-make.exe
-.\main.exe
-cd ..
-rm .\build\
