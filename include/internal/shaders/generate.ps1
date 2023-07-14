416886d81d06538a8a39a0980c793fa3be5ee2b9
OkiStuff <probro1802@gmail.com>
2023-07-14 19:39:52 -0400

    Shaders: Fixed generate.ps1 to add header guards

--- include/internal/shaders/generate.ps1
+++ include/internal/shaders/generate.ps1
@@ -1,4 +1,5 @@
 foreach ($File in (Get-ChildItem *.glsl).Name) {
-	xxd -i $File | Out-File -FilePath ".\$File.h" -Encoding "utf8"
-	Add-Content -Path ".\$File.h" -Value ("// This File was generated on {0} using xxd. Do not modify!" -f (Get-Date)) -Encoding "utf8"
+	$data = xxd -i $File
+	$full = "#ifndef MUZZLE_CORE_INTERNAL_SHADER_XXI_$($File.Replace(".", "_").ToUpper())_H`n#define MUZZLE_CORE_INTERNAL_SHADER_XXI_$($File.Replace(".", "_").ToUpper())_H`n`n$($data)`n`n#endif // This file was generated on $(Get-Date) using xxd. Do not modify!"
+	$full | Out-File -FilePath ".\$File.h" -Encoding "utf8"
 }
