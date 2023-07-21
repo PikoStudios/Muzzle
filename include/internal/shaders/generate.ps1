017787cc5995d240d9b3e5a549dc773f11cfc855
OkiStuff <probro1802@gmail.com>
2023-07-21 16:21:27 -0400

    Default Shaders: added default shaders

--- include/internal/shaders/generate.ps1
+++ include/internal/shaders/generate.ps1
@@ -1,5 +1,5 @@
 foreach ($File in (Get-ChildItem *.glsl).Name) {
 	$data = xxd -i $File
-	$full = "#ifndef MUZZLE_CORE_INTERNAL_SHADER_XXI_$($File.Replace(".", "_").ToUpper())_H`n#define MUZZLE_CORE_INTERNAL_SHADER_XXI_$($File.Replace(".", "_").ToUpper())_H`n`n$($data)`n`n#endif // This file was generated on $(Get-Date) using xxd. Do not modify!"
+	$full = "#ifndef MUZZLE_CORE_INTERNAL_SHADER_XXD_$($File.Replace(".", "_").ToUpper())_H`n#define MUZZLE_CORE_INTERNAL_SHADER_XXD_$($File.Replace(".", "_").ToUpper())_H`n`n$($data)`n`n#endif // This file was generated on $(Get-Date) using `generate.ps1`. Do not modify!"
 	$full | Out-File -FilePath ".\$File.h" -Encoding "utf8"
 }
