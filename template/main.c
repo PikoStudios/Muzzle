ebca98de82b7fd17355b004aabd49eb52d4c9d98
Frankie A <probro1802@gmail.com>
2025-03-22 23:43:12 -0400

    Muzzle now builds as a static library

---
+++ template/main.c
@@ -0,0 +1,23 @@
+#include <Muzzle.h>
+#include <stdio.h>
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+void applet_loop(mz_applet* applet)
+{
+    while (mz_keep_applet(applet))
+    {
+        mz_begin_drawing(applet);
+            mz_clear_screen(TINT_GRAY);
+        mz_end_drawing(applet);
+    }
+}
+
+int main(void)
+{
+    mz_applet applet = mz_initialize_applet("My Muzzle App", SCREEN_WIDTH, SCREEN_HEIGHT, APPLET_FLAG_RESIZBALE | APPLET_FLAG_VSYNC | APPLET_FLAG_TRACK_DELTA_TIME);
+    mz_start_applet(&applet, applet_loop);
+
+    mz_terminate_applet(&applet);
+    return 0;
+}
