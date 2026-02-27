1b0f2d0bc6ec25ec08baf71bdd2e71061636e098
Frankie A <probro1802@gmail.com>
2026-02-27 09:21:55 -0500

    Remove unneccesary pipeline argument in Java API

--- bindings/java/java/dev/pikostudios/muzzle/bridge/GraphicsPipeline.java
+++ bindings/java/java/dev/pikostudios/muzzle/bridge/GraphicsPipeline.java
@@ -93,12 +93,12 @@ public class GraphicsPipeline extends NativeStruct
 
 	private final static native GraphicsPipeline create(Shader shader, int[] colorAttachmentFormats, int framebufferWidth, int framebufferHeight, boolean createDepthBuffer, int depthBufferType);
 
-	public final void dispatch(GraphicsPipeline pipeline, VertexBuffer buffer, long start, long count)
+	public final void dispatch(VertexBuffer buffer, long start, long count)
 	{
 		assert start >= 0 && start <= 0xFFFFFFFFL : "start is out of uint32 range";
 		assert count >= 0 && count <= 0xFFFFFFFFL : "end is out of uint32 range";
 
-		this._dispatch(pipeline, buffer, start, count);
+		this._dispatch(buffer, start, count);
 	}
 	
 	public final void clearColorAttachments(Tint clearColor)
